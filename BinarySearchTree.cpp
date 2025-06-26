2c736430f1e9a956180f7909aa9091a1039c2052
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-26 18:37:40 +0700

    membuat step 7

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -66,11 +66,16 @@ public:
             return;
         }
 
+        // Step 7: if the value in the data field of the new node is greater than that of the parent
+        else if (x > parent->info)
+        {
+            // 7a: Make the right child of parent point to the new node
+            parent->rightchild = newNode;
 
-
-
+            // 7b: Exit
+            return;
+        }
     }
-
     
     
 };
