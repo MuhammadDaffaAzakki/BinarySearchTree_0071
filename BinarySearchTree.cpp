d5aa35bbf787d0bb58a68a6a33db3a30e69b5356
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-26 18:24:50 +0700

    menambahkan step 2, menambahkan value

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -35,7 +35,10 @@ public:
         //Step 1: Allocate memory for the new node
         Node *newNode = new Node();
 
+        //Step 2: Assign value to the data field of the new node
+        newNode->info = x;
 
+    }
 
     
     
