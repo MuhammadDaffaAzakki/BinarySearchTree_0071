0567766ed926dbf7542e39d31c860757b58c06e8
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-26 18:33:45 +0700

    membuat step 6

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -56,6 +56,17 @@ public:
             return;
         }
 
+        // Step 6: if the value in the data field of new node is less than that of parent
+        if (x < parent->info)
+        {
+            // 6a: Make the left child of parent point to new node
+            parent->leftchild = newNode;
+
+            // 6b: Exit
+            return;
+        }
+
+
 
 
     }
