46f1e757d99db9e3b1db5d35698cf426926a810d
MuhammadDaffaAzakki <daffaazakki29@gmail.com>
2025-06-26 18:23:52 +0700

    menambahkan step 1 membuat new node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -32,8 +32,10 @@ public:
         cout << "Masukkan nilai: ";
         cin >> x;
 
-        
-    }
+        //Step 1: Allocate memory for the new node
+        Node *newNode = new Node();
+
+
 
     
     
